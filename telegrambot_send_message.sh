9cf84b84254dde548e71494f1add99c0fa02e465
henschel <henschel@student.21-school.ru>
2023-04-05 17:11:44 +0000

    bot fix

--- telegrambot_send_message.sh
+++ telegrambot_send_message.sh
@@ -3,7 +3,7 @@
 # TELEGRAM_BOT_TOKEN = '6036727773:AAFVnhvI-ySiDQRMj2lFF5AAB74OLfQXido'
 
 URL="https://api.telegram.org/bot6036727773:AAFVnhvI-ySiDQRMj2lFF5AAB74OLfQXido/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$2%0AURL:+$3/pipelines/$4/%0ABranch:+$5"
+TEXT="Stage $1: $2%0A%0AProject:+$3%0AURL:+$4/pipelines/$5/%0ABranch:+$6"
 
 curl -s --max-time 30 -d "chat_id=457397290&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
 
