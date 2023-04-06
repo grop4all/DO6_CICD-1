e708ae2b9fb0e75d2a341b9c9efe28b88c337b29
henschel <henschel@student.21-school.ru>
2023-04-06 17:07:00 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp artifacts/s21_cat artifacts/s21_grep user@19.168.0.2:/home/user/
-ssh user@19.168.0.2 "echo user | sudo -S mv s21_cat s21_grep /usr/local/bin/"
+ssh root@19.168.0.2 "mv /home/user/s21_cat /home/user/s21_grep /usr/local/bin/"
