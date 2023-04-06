6fe15c7fb880ef28bbb74a7b67e26119d301c99f
henschel <henschel@student.21-school.ru>
2023-04-06 12:35:15 +0000

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/expect -f
-ls .
-spawn scp src/cat/s21_cat src/grep/s21_grep student@192.168.0.2:/home/student/
-
-expect {
-    "*yes/no" { send -- "yes\r"}
-    "*password" { send -- "student\r"}
-}
-spawn ssh student@19.168.0.2 "echo student | sudo -S mv s21_cat s21_grep /usr/local/bin/"
-expect "*password*"
-send -- "student\r"
-expect eof
+#!/bin/bash
+scp artifacts/s21_cat artifacts/s21_grep user@19.168.0.2:/home/user/
+ssh user@19.168.0.2 "echo user | sudo -S mv s21_cat s21_grep /usr/local/bin/"
